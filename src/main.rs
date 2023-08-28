d14cf3138c329309e6cfa496f4750000dd1e83e8
Vivien <bonnet.vivien@gmail.com>
2023-08-28 12:01:49 +0200

    OpenGL clear the window each frame.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod window;
 fn main() {
     let mut window = window::Window::create_window(800, 600, "Vorustious");
     while !window.should_close() {
+        window.clear();
         window.refresh();
         window.update_events();
     }
--- src/window.rs
+++ src/window.rs
@@ -1,3 +1,4 @@
+extern crate gl;
 extern crate glfw;
 
 use glfw::Context;
@@ -6,21 +7,39 @@ pub struct Window {
     core: glfw::Glfw,
     window: glfw::Window,
     events: std::sync::mpsc::Receiver<(f64, glfw::WindowEvent)>,
+
+    width: u32,
+    height: u32,
 }
 
 impl Window {
     pub fn create_window(width: u32, height: u32, title: &str) -> Window {
         let mut core = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+        core.window_hint(glfw::WindowHint::ContextVersion(3, 0));
+        core.window_hint(glfw::WindowHint::ClientApi(glfw::ClientApiHint::OpenGlEs));
+        core.window_hint(glfw::WindowHint::Resizable(false));
+
         let (mut window, events) = core.create_window(width, height, title, glfw::WindowMode::Windowed)
             .expect("Failed to create GLFW window.");
 
         window.make_current();
         window.set_key_polling(true);
+        gl::load_with(|ptr| window.get_proc_address(ptr) as *const _);
         
         Self {
             core: core,
             window: window,
             events: events,
+            width: width,
+            height: height,
+        }
+    }
+
+    pub fn clear(&self) {
+        unsafe {
+            gl::Viewport(0, 0, self.width.try_into().unwrap(), self.height.try_into().unwrap());
+            gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
         }
     }
 
