8b114dc2c51aa07154e74a670b8ce7f6ca4d7d0c
Vivien <bonnet.vivien@gmail.com>
2023-08-28 16:49:56 +0200

    Creation of a VertexArrayObject and display of a triangle.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 mod shader;
+mod vertex_objects;
 mod window;
 
 use shader::Shader;
+use vertex_objects::VertexArrayObject;
 
 fn main() {
     let mut window = window::Window::create_window(800, 600, "Vorustious");
 
     let shader = Shader::create_shader_program("shaders/hello_triangle.vert", "shaders/hello_triangle.frag");
 
+    let vertices: Vec<f32> = [
+        -0.5, 0.5, 0.0,
+        0.5, 0.5, 0.0,
+        0.0, -0.5, 0.0,
+    ].to_vec();
+    let mut vao = VertexArrayObject::create();
+    vao.set_vertices(vertices);
+
     while !window.should_close() {
         window.clear();
-        window.refresh();
-        window.update_events();
 
         shader.use_program();
+        vao.draw();
+
+        window.refresh();
+        window.update_events();
     }
 }
---
+++ src/vertex_objects.rs
@@ -0,0 +1,61 @@
+extern crate gl;
+
+pub struct VertexArrayObject {
+    id: gl::types::GLuint,
+    buffer_objects: Vec<gl::types::GLuint>,
+}
+
+impl VertexArrayObject {
+    pub fn create() -> VertexArrayObject {
+        let mut vao = 0;
+        unsafe { gl::GenVertexArrays(1, &mut vao) };
+        VertexArrayObject {
+            id: vao,
+            buffer_objects: vec![],
+        }
+    }
+
+    pub fn set_vertices(&mut self, vertices_data: Vec<f32>) {
+        let size_in_bytes = (std::mem::size_of::<f32>() * vertices_data.len()).try_into().unwrap();
+        unsafe {
+            self.bind();
+            let mut vbo = 0;
+            gl::GenBuffers(1, &mut vbo);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(gl::ARRAY_BUFFER, size_in_bytes, vertices_data.as_ptr().cast(), gl::STATIC_DRAW);
+
+            gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 0, std::ptr::null::<_>());
+            gl::EnableVertexAttribArray(0);
+
+            self.buffer_objects.push(vbo);
+            self.unbind();
+        }
+    }
+
+    pub fn draw(&self) {
+        unsafe {
+            self.bind();
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            self.unbind();
+        }
+    }
+
+    unsafe fn bind(&self) {
+        gl::BindVertexArray(self.id);
+    }
+
+    unsafe fn unbind(&self) {
+        gl::BindVertexArray(0);
+    }
+}
+
+impl Drop for VertexArrayObject {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteVertexArrays(1, &self.id);
+            for vbo in &self.buffer_objects {
+                gl::DeleteBuffers(1, vbo);
+            }
+        }
+    }
+}
