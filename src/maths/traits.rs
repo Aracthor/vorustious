9c062db24e190d121c398875db9f61d52ba6ae1f
Vivien <bonnet.vivien@gmail.com>
2023-09-02 13:29:02 +0200

    Using a double specification rather than adding a depedency to trait-set just for an alias un traits.rs.

--- src/maths/traits.rs
+++ src/maths/traits.rs
@@ -1,18 +1,35 @@
-trait_set::trait_set! {
-    pub trait MathsUsable =
-        Copy +
-        From<i8> +
-        num_traits::Bounded +
-        std::cmp::PartialEq<Self> +
-        std::cmp::PartialOrd<Self> +
-        std::ops::Neg<Output = Self> +
-        std::ops::Add<Self, Output = Self> +
-        std::ops::Sub<Self, Output = Self> +
-        std::ops::Mul<Self, Output = Self> +
-        std::ops::Div<Self, Output = Self> +
-        std::ops::AddAssign<Self> +
-        std::ops::SubAssign<Self> +
-        std::ops::MulAssign<Self> +
-        std::ops::DivAssign<Self> +
-        std::fmt::Display
-}
+pub trait MathsUsable:
+    Copy +
+    From<i8> +
+    num_traits::Bounded +
+    std::cmp::PartialEq<Self> +
+    std::cmp::PartialOrd<Self> +
+    std::ops::Neg<Output = Self> +
+    std::ops::Add<Self, Output = Self> +
+    std::ops::Sub<Self, Output = Self> +
+    std::ops::Mul<Self, Output = Self> +
+    std::ops::Div<Self, Output = Self> +
+    std::ops::AddAssign<Self> +
+    std::ops::SubAssign<Self> +
+    std::ops::MulAssign<Self> +
+    std::ops::DivAssign<Self> +
+    std::fmt::Display
+{}
+
+impl<T:
+    Copy +
+    From<i8> +
+    num_traits::Bounded +
+    std::cmp::PartialEq<Self> +
+    std::cmp::PartialOrd<Self> +
+    std::ops::Neg<Output = Self> +
+    std::ops::Add<Self, Output = Self> +
+    std::ops::Sub<Self, Output = Self> +
+    std::ops::Mul<Self, Output = Self> +
+    std::ops::Div<Self, Output = Self> +
+    std::ops::AddAssign<Self> +
+    std::ops::SubAssign<Self> +
+    std::ops::MulAssign<Self> +
+    std::ops::DivAssign<Self> +
+    std::fmt::Display> MathsUsable
+for T {}
