1aeff147a2caac597f8db5156a170c56443d50bc
Vivien <bonnet.vivien@gmail.com>
2023-12-05 22:09:58 +0100

    Added a couple of tests about projectile damage.

--- src/warfare/unit_tests.rs
+++ src/warfare/unit_tests.rs
@@ -1,5 +1,13 @@
+use crate::maths::matrix::Mat4f;
 use crate::maths::vector::Vect3f;
+use crate::maths::vector::Vect3i;
+use crate::voxels::body::Body;
+use crate::voxels::structure::Structure;
+use crate::voxels::voxel::Voxel;
+use crate::voxels::voxel::VoxelID;
+use super::battle::Battle;
 use super::projectile::Projectile;
+
 
 #[test]
 fn projectile_movement() {
@@ -25,4 +33,53 @@ fn projectile_max_distance() {
     assert!(!projectile.is_out_of_max_range());
     projectile.moove(1.0);
     assert!(projectile.is_out_of_max_range());
+}
+
+#[test]
+fn projectile_damage() {
+    let mut battle = Battle::new();
+    let structure = Structure::new(-1, 1, -1, 1, -1, 1, Voxel{life: 2.0, id: VoxelID::LightHull});
+    battle.add_body(Body::new(structure, Mat4f::identity()));
+
+    let initial_position = Vect3f::new([-10.0, 1.0, 1.0]);
+    let movement = Vect3f::new([10.0, 0.0, 0.0]);
+    battle.add_projectile(Projectile::new(initial_position, movement, 1.0, f32::MAX));
+    battle.update(1.0);
+    assert!(battle.bodies()[0].structure().get_voxel(Vect3i::new([-1, 1, 1])).unwrap().life == 1.0);
+
+    battle.add_projectile(Projectile::new(initial_position, movement, 1.0, f32::MAX));
+    battle.update(1.0);
+    assert!(battle.bodies()[0].structure().get_voxel(Vect3i::new([-1, 1, 1])).is_none());
+}
+
+#[test]
+fn projectile_damage_on_moving_body() {
+    let mut battle = Battle::new();
+    let structure = Structure::new(-1, 1, -1, 1, -1, 1, Voxel{life: 2.0, id: VoxelID::LightHull});
+    let mut expected_structure = structure.clone();
+    let mut body = Body::new(structure, Mat4f::identity());
+    body.add_to_movement(Vect3f::new([0.0, 0.4, 0.0]));
+    battle.add_body(body);
+
+    let initial_position = Vect3f::new([-10.0, 0.0, 0.0]);
+    let movement = Vect3f::new([10.0, 0.0, 0.0]);
+    battle.add_projectile(Projectile::new(initial_position, movement, 1.0, f32::MAX));
+    battle.update(1.0);
+    expected_structure.set_voxel(-1, 0, 0, Some(Voxel{life: 1.0, id: VoxelID::LightHull}));
+    assert!(battle.bodies()[0].structure().clone() == expected_structure);
+
+    battle.add_projectile(Projectile::new(initial_position, movement, 1.0, f32::MAX));
+    battle.update(1.0);
+    expected_structure.set_voxel(-1, -1, 0, Some(Voxel{life: 1.0, id: VoxelID::LightHull}));
+    assert!(battle.bodies()[0].structure().clone() == expected_structure);
+
+    battle.add_projectile(Projectile::new(initial_position, movement, 1.0, f32::MAX));
+    battle.update(1.0);
+    expected_structure.set_voxel(-1, -1, 0, None);
+    assert!(battle.bodies()[0].structure().clone() == expected_structure);
+
+    battle.add_projectile(Projectile::new(initial_position, movement, 1.0, f32::MAX));
+    battle.update(1.0);
+    // Nothing new, the projectile should have missed.
+    assert!(battle.bodies()[0].structure().clone() == expected_structure);
 }
