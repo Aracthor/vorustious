3520d880f188cf6850a81a0102a4e6c57761146d
Vivien <bonnet.vivien@gmail.com>
2025-06-21 19:37:10 +0200

    Slightly simpler collision new velocity calculation.

--- src/physics/collision.rs
+++ src/physics/collision.rs
@@ -86,12 +86,9 @@ pub fn apply_collision_if_any(body_a: &mut Body, body_b: &mut Body, restitution:
         let momentum_b = body_b.momentum(average_intersection_b);
         let mass_a = body_a.structure().mass();
         let mass_b = body_b.structure().mass();
-        let velocity_a = body_a.velocity();
-        let velocity_b = body_b.velocity();
-        let total_mass = mass_a + mass_b;
-
-        let new_velocity_a = (momentum_a + momentum_b + (velocity_b - velocity_a) * mass_b * restitution) / total_mass;
-        let new_velocity_b = (momentum_a + momentum_b + (velocity_a - velocity_b) * mass_a * restitution) / total_mass;
+        let center_velocity = (momentum_a + momentum_b) / (mass_a + mass_b);
+        let new_velocity_a = center_velocity * (1.0 + restitution) - body_a.velocity() * restitution;
+        let new_velocity_b = center_velocity * (1.0 + restitution) - body_b.velocity() * restitution;
 
         body_a.set_velocity(new_velocity_a);
         body_b.set_velocity(new_velocity_b);
